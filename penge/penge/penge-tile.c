54e4bc844b3b8a0041f726dca52dfc4dd1c73c99
Rob Bradford <rob@linux.intel.com>
2008-12-05 15:43:04 +0000

    Tweak the tile sizing policy
    If the tile is explicitly sized then the child will be centered inside the
    tile relative to the frame margins otherwise the tile will surround the child
    with appropriate margins and padding.

--- penge/penge/penge-tile.c
+++ penge/penge/penge-tile.c
@@ -14,13 +14,18 @@ struct _PengeTilePrivate {
     ClutterActor *child;
 };
 
-#define PADDING 10
+#define PADDING 0
 #define BG_FILENAME "data/frame-bg.png"
 
 #define FRAME_LEFT 7
 #define FRAME_TOP 5
 #define FRAME_RIGHT 64
 #define FRAME_BOTTOM 58
+
+#define MARGIN_LEFT 3
+#define MARGIN_TOP 3
+#define MARGIN_RIGHT 10
+#define MARGIN_BOTTOM 8
 
 enum {
   PROP0,
@@ -113,6 +118,9 @@ _tile_allocate (ClutterActor          *actor,
 {
   PengeTilePrivate *priv = GET_PRIVATE (actor);
   gint w, h;
+  ClutterUnit child_wu, child_hu;
+  ClutterActorBox child_box;
+  ClutterActorBox inner_box;
 
   /* chain up to set the allocation of the actor */
   CLUTTER_ACTOR_CLASS (penge_tile_parent_class)->allocate (actor, 
@@ -121,17 +129,110 @@ _tile_allocate (ClutterActor          *actor,
 
   w = CLUTTER_UNITS_TO_DEVICE (box->x2 - box->x1);
   h = CLUTTER_UNITS_TO_DEVICE (box->y2 - box->y1);
+
+  /* 
+   * Represents the maximum inner space available to the child including
+   * framing and padding
+   */
+  inner_box.x1 = CLUTTER_UNITS_FROM_DEVICE (MARGIN_LEFT + PADDING);
+  inner_box.y1 = CLUTTER_UNITS_FROM_DEVICE (MARGIN_TOP + PADDING);
+  inner_box.x2 = CLUTTER_UNITS_FROM_DEVICE (w - (MARGIN_RIGHT + PADDING));
+  inner_box.y2 = CLUTTER_UNITS_FROM_DEVICE (h - (MARGIN_BOTTOM + PADDING));
+
+  /* The frame should always match the size of the tile */
   clutter_actor_set_size (priv->frame, w, h);
 
   if (priv->child)
   {
-    clutter_actor_set_size (priv->child,
-                            w - (2 * PADDING),
-                            h - (2 * PADDING));
-    clutter_actor_allocate_preferred_size (priv->child, origin_changed);
-  }
+    /* 
+     * Get the child size and then it's preferred width and height and then
+     * center it inside the inner box, which is thus relative to the tile
+     */
+    clutter_actor_get_preferred_width (priv->child, -1, NULL, &child_wu);
+    clutter_actor_get_preferred_height (priv->child, -1, NULL, &child_hu);
 
-  g_debug (G_STRLOC ": Allocate called. Setting size to %d %d", w, h);
+    child_box.x1 = inner_box.x1 + (((inner_box.x2 - inner_box.x1) - child_wu) / 2);
+    child_box.x2 = child_box.x1 + child_wu;
+
+    child_box.y1 = inner_box.y1 + (((inner_box.y2 - inner_box.y1) - child_hu) / 2);
+    child_box.y2 = child_box.y1 + child_hu;
+
+    clutter_actor_allocate (priv->child, &child_box, origin_changed);
+  }
+}
+
+static void
+_tile_get_preferred_width (ClutterActor *actor,
+                           ClutterUnit   for_height,
+                           ClutterUnit  *min_width_p,
+                           ClutterUnit  *nat_width_p)
+{
+  PengeTilePrivate *priv = GET_PRIVATE (actor);
+
+  ClutterUnit min_child_width;
+  ClutterUnit nat_child_width;
+
+  if (priv->child)
+  {
+    clutter_actor_get_preferred_width (priv->child,
+                                       for_height,
+                                       &min_child_width,
+                                       &nat_child_width);
+
+    *min_width_p = min_child_width + 
+                   CLUTTER_UNITS_FROM_DEVICE (MARGIN_LEFT) +
+                   CLUTTER_UNITS_FROM_DEVICE (MARGIN_RIGHT) +
+                   CLUTTER_UNITS_FROM_DEVICE (PADDING * 2);
+
+
+    *nat_width_p = nat_child_width + 
+                   CLUTTER_UNITS_FROM_DEVICE (MARGIN_LEFT) +
+                   CLUTTER_UNITS_FROM_DEVICE (MARGIN_RIGHT) +
+                   CLUTTER_UNITS_FROM_DEVICE (PADDING * 2);
+  } else {
+    /* Just use the size of the frame */
+    clutter_actor_get_preferred_width (priv->frame,
+                                       for_height,
+                                       min_width_p,
+                                       nat_width_p);
+  }
+}
+
+static void
+_tile_get_preferred_height (ClutterActor *actor,
+                            ClutterUnit   for_width,
+                            ClutterUnit  *min_height_p,
+                            ClutterUnit  *nat_height_p)
+{
+  PengeTilePrivate *priv = GET_PRIVATE (actor);
+
+  ClutterUnit min_child_height;
+  ClutterUnit nat_child_height;
+
+  if (priv->child)
+  {
+    clutter_actor_get_preferred_height (priv->child,
+                                       for_width,
+                                       &min_child_height,
+                                       &nat_child_height);
+
+    *min_height_p = min_child_height + 
+                   CLUTTER_UNITS_FROM_DEVICE (MARGIN_TOP) +
+                   CLUTTER_UNITS_FROM_DEVICE (MARGIN_BOTTOM) +
+                   CLUTTER_UNITS_FROM_DEVICE (PADDING * 2);
+
+
+    *nat_height_p = nat_child_height + 
+                   CLUTTER_UNITS_FROM_DEVICE (MARGIN_TOP) +
+                   CLUTTER_UNITS_FROM_DEVICE (MARGIN_BOTTOM) +
+                   CLUTTER_UNITS_FROM_DEVICE (PADDING * 2);
+  } else {
+    /* Just use the size of the frame */
+    clutter_actor_get_preferred_height (priv->frame,
+                                       for_width,
+                                       min_height_p,
+                                       nat_height_p);
+  }
 }
 
 static void
@@ -150,6 +251,8 @@ penge_tile_class_init (PengeTileClass *klass)
 
   actor_class->allocate = _tile_allocate;
   actor_class->paint = _tile_paint;
+  actor_class->get_preferred_width = _tile_get_preferred_width;
+  actor_class->get_preferred_height = _tile_get_preferred_height;
 
   pspec = g_param_spec_object ("child",
                                "child",
