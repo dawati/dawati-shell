7b169e89dfe84613917f45484ebc5ebc3e8f2c3f
Rob Bradford <rob@linux.intel.com>
2009-01-07 12:17:33 +0000

    Add a test for the grid view.

---
+++ penge/examples/test-grid-view.c
@@ -0,0 +1,25 @@
+#include "penge-grid-view.h"
+
+int
+main (int    argc,
+      char **argv)
+{
+  ClutterActor *stage;
+  ClutterActor *grid;
+
+  clutter_init (&argc, &argv);
+
+  stage = clutter_stage_get_default ();
+  clutter_actor_set_size (stage, 1024, 600);
+
+  grid = g_object_new (PENGE_TYPE_GRID_VIEW,
+                       NULL);
+  clutter_actor_set_size (grid, 1024, 500);
+  clutter_container_add_actor (CLUTTER_CONTAINER (stage), grid);
+  clutter_actor_set_position (grid, 0, 50);
+  clutter_actor_show_all (stage);
+
+  clutter_main ();
+
+  return 0;
+}
