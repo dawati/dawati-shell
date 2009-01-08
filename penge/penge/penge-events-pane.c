2666087ffedec3c55a11b3b6bf3ab55485bae5e6
Rob Bradford <rob@linux.intel.com>
2009-01-08 17:47:41 +0000

    Set padding on the events pane.

--- penge/penge/penge-events-pane.c
+++ penge/penge/penge-events-pane.c
@@ -337,6 +337,10 @@ static void
 penge_events_pane_init (PengeEventsPane *self)
 {
   PengeEventsPanePrivate *priv = GET_PRIVATE (self);
+  NbtkPadding padding = { CLUTTER_UNITS_FROM_DEVICE (8),
+                          CLUTTER_UNITS_FROM_DEVICE (8),
+                          CLUTTER_UNITS_FROM_DEVICE (8),
+                          CLUTTER_UNITS_FROM_DEVICE (8) };
 
   /* Create hashes to store our view membership in */
   priv->uid_to_events = g_hash_table_new_full (g_str_hash,
@@ -358,4 +362,6 @@ penge_events_pane_init (PengeEventsPane *self)
                     (GCallback)_store_opened_cb,
                     self);
   jana_store_open (priv->store);
+
+  nbtk_widget_set_padding (NBTK_WIDGET (self), &padding);
 }
