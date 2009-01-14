1ed77798928768dff873100b719169525ad1188a
Tomas Frydrych <tf@o-hand.com>
2009-01-14 12:50:27 +0000

    Modified test-tray so that config window can be shown more than once.

--- tests/test-tray.c
+++ tests/test-tray.c
@@ -35,6 +35,16 @@ button_clicked_cb (GtkButton *button, gpointer data)
   gtk_widget_hide (config);
 }
 
+/*
+ * Re-enable showing of the widget once we are removed from the socket.
+ */
+static gboolean
+plug_show_on_delete (GtkWidget *widget)
+{
+  gtk_widget_show_all (widget);
+  return TRUE;
+}
+
 int
 main (int argc, char *argv[])
 {
@@ -46,6 +56,17 @@ main (int argc, char *argv[])
   table = gtk_table_new (3, 3, TRUE);
 
   config = gtk_plug_new (0);
+
+  /*
+   * When we hide the config, the tray manager will remove the window
+   * from it's container; this triggers the delete-event; we want to (a) stop
+   * this event from destroying our plug (so we can show it again) and (b)
+   * call _show_all() on the plug, to make sure that when it is needed again
+   * it can be made visible by the socket.
+   */
+  g_signal_connect (config, "delete-event",
+                    G_CALLBACK (plug_show_on_delete), NULL);
+
   gtk_container_add (GTK_CONTAINER (config), table);
 
   button = gtk_button_new_from_stock (GTK_STOCK_QUIT);
