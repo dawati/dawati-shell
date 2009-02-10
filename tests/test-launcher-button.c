360edfb6227e490565c3289eaf81d88d1696a408
Robert Staudinger <robsta@gnome.org>
2009-02-10 16:59:08 +0100

    Add test snippet for MnbLauncherButton.
    Adapt build infrastructure under tests/ so standalone tests can be built.

---
+++ tests/test-launcher-button.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <clutter/clutter.h>
+#include "mnb-launcher-button.h"
+
+static void
+clicked_cb (NbtkWidget          *widget,
+            ClutterButtonEvent  *event,
+            gpointer             data)
+{
+  printf ("%s() %d\n", __FUNCTION__, event->time);
+}
+
+int
+main (int argc, char *argv[])
+{
+  NbtkWidget *launcher;
+  ClutterColor stage_color =  { 0xff, 0xff, 0xff, 0xff };
+  ClutterActor *stage;
+  GError *err = NULL;
+
+  clutter_init (&argc, &argv);
+
+  nbtk_style_load_from_file (nbtk_style_get_default (),
+                             "../data/theme/mutter-moblin.css", NULL);
+
+  stage = clutter_stage_get_default ();
+  clutter_actor_set_size (stage, 400, 200);
+
+  launcher = mnb_launcher_button_new ("../data/theme/panel/internet-active.png",
+                                      32, "Launcher Button", "Test", "Comment");
+  clutter_actor_set_position (CLUTTER_ACTOR (launcher), 50, 50);
+  clutter_container_add (CLUTTER_CONTAINER (stage), CLUTTER_ACTOR (launcher), NULL);
+  g_signal_connect (launcher, "activated", G_CALLBACK (clicked_cb), NULL);
+
+  clutter_actor_show (stage);
+
+  clutter_main ();
+
+  return EXIT_SUCCESS;
+}
