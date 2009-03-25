fdb3390c1fa8c9886718bd5bd1bbfb8209f87288
Rob Bradford <rob@linux.intel.com>
2009-03-25 18:39:50 +0000

    [penge] Remove animation on favourite apps and instead use the hover style

--- penge/penge/penge-app-tile.c
+++ penge/penge/penge-app-tile.c
@@ -15,9 +15,6 @@ typedef struct _PengeAppTilePrivate PengeAppTilePrivate;
 struct _PengeAppTilePrivate {
   PengeAppBookmark *bookmark;
   ClutterActor *tex;
-  ClutterActor *underlay;
-  ClutterTimeline *timeline;
-  ClutterBehaviour *behave;
 };
 
 enum
@@ -140,15 +137,8 @@ _enter_event_cb (ClutterActor *actor,
                  ClutterEvent *event,
                  gpointer      userdata)
 {
-  PengeAppTilePrivate *priv = GET_PRIVATE (userdata);
-
-  clutter_timeline_set_direction (priv->timeline,
-                                  CLUTTER_TIMELINE_FORWARD);
-  if (!clutter_timeline_is_playing (priv->timeline))
-  {
-    clutter_timeline_rewind (priv->timeline);
-    clutter_timeline_start (priv->timeline);
-  }
+  nbtk_widget_set_style_pseudo_class (NBTK_WIDGET (actor),
+                                      "hover");
 
   return FALSE;
 }
@@ -158,12 +148,8 @@ _leave_event_cb (ClutterActor *actor,
                  ClutterEvent *event,
                  gpointer      userdata)
 {
-  PengeAppTilePrivate *priv = GET_PRIVATE (userdata);
-
-  clutter_timeline_set_direction (priv->timeline,
-                                  CLUTTER_TIMELINE_BACKWARD);
-  if (!clutter_timeline_is_playing (priv->timeline))
-    clutter_timeline_start (priv->timeline);
+  nbtk_widget_set_style_pseudo_class (NBTK_WIDGET (actor),
+                                      NULL);
 
   return FALSE;
 }
@@ -207,26 +193,6 @@ static void
 penge_app_tile_init (PengeAppTile *self)
 {
   PengeAppTilePrivate *priv = GET_PRIVATE (self);
-
-  ClutterAlpha *alpha;
-  ClutterColor black = { 0x0, 0x0, 0x0, 0xff };
-
-  /* For mouse over effect that darkens the background we need this magic
-   * rectangle
-   */
-  priv->underlay = clutter_rectangle_new_with_color (&black);
-  clutter_actor_set_opacity (priv->underlay, 0x0);
-  nbtk_table_add_actor (NBTK_TABLE (self),
-                        priv->underlay,
-                        0,
-                        0);
-  priv->timeline = clutter_timeline_new_for_duration (300);
-
-  alpha = clutter_alpha_new_full (priv->timeline,
-                                  CLUTTER_LINEAR);
-  priv->behave = clutter_behaviour_opacity_new (alpha, 0x00, 0x60);
-  clutter_behaviour_apply (priv->behave,
-                           (ClutterActor *)priv->underlay);
 
   priv->tex = clutter_texture_new ();
   nbtk_table_add_actor (NBTK_TABLE (self),
