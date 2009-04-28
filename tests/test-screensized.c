d3ed46dcfe6355d2031ae3545547795ebf5616cc
Tomas Frydrych <tf@linux.intel.com>
2009-04-28 16:55:20 +0100

    [test-screensize] Simple test for screen-sized, undecorated apps.

---
+++ tests/test-screensized.c
@@ -0,0 +1,55 @@
+/* -*- mode: C; c-file-style: "gnu"; indent-tabs-mode: nil; -*- */
+
+/*
+ * Copyright (c) 2008 Intel Corp.
+ *
+ * Author: Tomas Frydrych <tf@linux.intel.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ */
+
+/*
+ * Simple test for screen sized, undecorated windows.
+ *
+ */
+
+#include <gtk/gtk.h>
+
+int
+main (int argc, char *argv[])
+{
+  GtkWidget *window;
+  GtkWidget *label;
+
+  gtk_init (&argc, &argv);
+
+  window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+  gtk_window_set_decorated (GTK_WINDOW (window), FALSE);
+
+  gtk_window_set_default_size (GTK_WINDOW (window),
+                               gdk_screen_width (),
+                               gdk_screen_height ());
+
+  label = gtk_label_new ("Screen-sized, undecorated is not fullscreen !!!");
+
+  gtk_container_add (GTK_CONTAINER (window), label);
+
+  gtk_widget_show_all (window);
+
+  gtk_main ();
+
+  return 0;
+}
