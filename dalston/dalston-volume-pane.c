c98f2f8f9f42a138e0c21420e063c00a9b61e9d9
Robert Staudinger <robsta@gnome.org>
2009-05-14 22:05:24 +0100

    Use light-switch buttons and gconf monitoring.
    Change the "mute" setting to use nbtk-gtk-light-switch.
    
    Add an "alert sounds (on/off)" light-switch that is connected to the
    associated gconf key. Requires linking libdalston and
    dalston-volume-applet against gconf.

--- dalston/dalston-volume-pane.c
+++ dalston/dalston-volume-pane.c
@@ -4,6 +4,10 @@
 #include <glib/gi18n.h>
 #include <canberra-gtk.h>
 #include <nbtk/nbtk-gtk.h>
+#include <gconf/gconf-client.h>
+
+#define EVENT_SOUNDS_DIRECTORY "/desktop/gnome/sound"
+#define EVENT_SOUNDS_KEY       EVENT_SOUNDS_DIRECTORY "/event_sounds"
 
 G_DEFINE_TYPE (DalstonVolumePane, dalston_volume_pane, GTK_TYPE_HBOX)
 
@@ -13,10 +17,13 @@ G_DEFINE_TYPE (DalstonVolumePane, dalston_volume_pane, GTK_TYPE_HBOX)
 typedef struct _DalstonVolumePanePrivate DalstonVolumePanePrivate;
 
 struct _DalstonVolumePanePrivate {
+  GConfClient *client;
   GvcMixerStream *sink;
   GtkWidget *mute_button;
+  GtkWidget *alert_sounds_button;
   GtkWidget *test_sound_button;
   GtkWidget *volume_slider;
+  guint gconf_connection_id;
 
 #if 0
   ca_context *ca_context;
@@ -71,6 +78,17 @@ dalston_volume_pane_dispose (GObject *object)
 {
   DalstonVolumePane *pane = (DalstonVolumePane *)object;
   DalstonVolumePanePrivate *priv = GET_PRIVATE (pane);
+
+  if (priv->client)
+  {
+    if (priv->gconf_connection_id)
+      {
+        gconf_client_notify_remove (priv->client, priv->gconf_connection_id);
+        priv->gconf_connection_id = 0;
+      }
+    g_object_unref (priv->client);
+    priv->client = NULL;
+  }
 
   if (priv->sink)
   {
@@ -129,15 +147,61 @@ _test_sound_button_release_cb (GtkWidget      *widget,
 }
 
 static void
-_mute_button_toggled_cb (GtkToggleButton *toggle_button,
-                         gpointer         userdata)
+_mute_button_switch_flipped_cb (NbtkGtkLightSwitch *light_switch,
+                                gboolean            state,
+                                DalstonVolumePane  *self)
 {
-  DalstonVolumePanePrivate *priv = GET_PRIVATE (userdata);
-  gboolean toggled;
+  DalstonVolumePanePrivate *priv = GET_PRIVATE (self);
 
-  toggled = gtk_toggle_button_get_active (toggle_button);
-  gvc_mixer_stream_change_is_muted (priv->sink, toggled);
-  gtk_widget_set_sensitive (priv->volume_slider, !toggled);
+  gvc_mixer_stream_change_is_muted (priv->sink, state);
+  gtk_widget_set_sensitive (priv->volume_slider, !state);
+}
+
+static void
+_alert_sounds_button_switch_flipped_cb (NbtkGtkLightSwitch *light_switch,
+                                        gboolean            state,
+                                        DalstonVolumePane  *self)
+{
+  DalstonVolumePanePrivate *priv = GET_PRIVATE (self);
+  GError *error = NULL;
+
+  gconf_client_set_bool (priv->client, EVENT_SOUNDS_KEY, state, &error);
+  if (error)
+  {
+    g_warning (G_STRLOC " %s", error->message);
+    g_clear_error (&error);
+  }
+}
+
+static void
+_set_alert_sounds_button_active (DalstonVolumePane *self,
+                                 gboolean active)
+{
+  DalstonVolumePanePrivate *priv = GET_PRIVATE (self);
+
+  /* Block the emission of the switch-flipped signal */
+  g_signal_handlers_block_by_func (priv->alert_sounds_button,
+                                   _alert_sounds_button_switch_flipped_cb,
+                                   self);
+
+  nbtk_gtk_light_switch_set_active (NBTK_GTK_LIGHT_SWITCH (priv->alert_sounds_button),
+                                    active);
+
+  g_signal_handlers_unblock_by_func (priv->alert_sounds_button,
+                                     _alert_sounds_button_switch_flipped_cb,
+                                     self);
+}
+
+static void
+_alert_sounds_button_update_cb (GConfClient *client,
+                                guint cnxn_id,
+                                GConfEntry *entry,
+                                DalstonVolumePane *self)
+{
+  GConfValue *value = gconf_entry_get_value (entry);
+
+  _set_alert_sounds_button_active (self,
+                                   gconf_value_get_bool (value));
 }
 
 static void
@@ -148,6 +212,12 @@ dalston_volume_pane_init (DalstonVolumePane *self)
   GtkWidget *label;
   GtkWidget *frame;
   GtkWidget *hbox;
+  GtkWidget *table;
+  GtkWidget *align;
+  GError    *error = NULL;
+  gboolean   alert_sounds_button_active;
+
+  priv->client = gconf_client_get_default ();
 
   gtk_box_set_spacing (GTK_BOX (self), 4);
   gtk_container_set_border_width (GTK_CONTAINER (self), 4);
@@ -155,7 +225,7 @@ dalston_volume_pane_init (DalstonVolumePane *self)
   frame = nbtk_gtk_frame_new ();
   vbox = gtk_vbox_new (FALSE, 8);
   gtk_container_set_border_width (GTK_CONTAINER (vbox), 4);
-  gtk_container_add (GTK_BIN (frame), vbox);
+  gtk_container_add (GTK_CONTAINER (frame), vbox);
   label = gtk_label_new (_("<span font_size=\"x-large\">Output volume</span>"));
   gtk_label_set_use_markup (GTK_LABEL (label), TRUE);
   gtk_misc_set_alignment (GTK_MISC (label), 0.0, 0.5);
@@ -184,34 +254,93 @@ dalston_volume_pane_init (DalstonVolumePane *self)
                       0);
 
   frame = nbtk_gtk_frame_new ();
-  vbox = gtk_vbox_new (FALSE, 8);
-  gtk_container_set_border_width (GTK_CONTAINER (vbox), 8);
-  gtk_container_add (GTK_BIN (frame), vbox);
-  priv->mute_button = gtk_toggle_button_new_with_label (_("Mute"));
+  table = gtk_table_new (3, 2, FALSE);
+  gtk_table_set_col_spacings (GTK_TABLE (table), 32);
+  gtk_table_set_row_spacings (GTK_TABLE (table), 16);
+  gtk_container_set_border_width (GTK_CONTAINER (table), 8);
+  gtk_container_add (GTK_CONTAINER (frame), table);
+
+  align = gtk_alignment_new (0., 0.5, 0., 0.);
+  gtk_table_attach (GTK_TABLE (table), align,
+                    0, 1, 0, 1,
+                    GTK_EXPAND | GTK_FILL, GTK_EXPAND | GTK_FILL,
+                    0, 0);
+  label = gtk_label_new (_("Mute"));
+  gtk_container_add (GTK_CONTAINER (align), label);
+
+  priv->mute_button = nbtk_gtk_light_switch_new ();
   g_signal_connect (priv->mute_button,
-                    "toggled",
-                    (GCallback)_mute_button_toggled_cb,
+                    "switch-flipped",
+                    (GCallback)_mute_button_switch_flipped_cb,
                     self);
-  gtk_box_pack_start (GTK_BOX (vbox),
-                      priv->mute_button,
-                      TRUE,
-                      FALSE,
-                      8);
+  gtk_table_attach (GTK_TABLE (table), priv->mute_button,
+                    1, 2, 0, 1,
+                    GTK_EXPAND | GTK_FILL, GTK_EXPAND | GTK_FILL,
+                    0, 0);
+
+  align = gtk_alignment_new (0., 0.5, 0., 0.);
+  gtk_table_attach (GTK_TABLE (table), align,
+                    0, 1, 1, 2,
+                    GTK_EXPAND | GTK_FILL, GTK_EXPAND | GTK_FILL,
+                    0, 0);
+  label = gtk_label_new (_("Alert sounds"));
+  gtk_container_add (GTK_CONTAINER (align), label);
+
+  priv->alert_sounds_button = nbtk_gtk_light_switch_new ();
+  g_signal_connect (priv->alert_sounds_button,
+                    "switch-flipped",
+                    (GCallback)_alert_sounds_button_switch_flipped_cb,
+                    self);
+  gtk_table_attach_defaults (GTK_TABLE (table),
+                             priv->alert_sounds_button,
+                             1, 2, 1, 2);
+
   priv->test_sound_button = gtk_button_new_with_label (_("Play test sound"));
   g_signal_connect (priv->test_sound_button,
                     "button-release-event",
                     (GCallback)_test_sound_button_release_cb,
                     self);
-  gtk_box_pack_start (GTK_BOX (vbox),
-                      priv->test_sound_button,
-                      TRUE,
-                      FALSE,
-                      8);
+  gtk_table_attach_defaults (GTK_TABLE (table), priv->test_sound_button,
+                             0, 2, 2, 3);
+
   gtk_box_pack_start (GTK_BOX (self),
                       frame,
                       FALSE,
                       FALSE,
                       0);
+
+  priv->gconf_connection_id = gconf_client_notify_add (
+                           priv->client,
+                           EVENT_SOUNDS_DIRECTORY,
+                           (GConfClientNotifyFunc)_alert_sounds_button_update_cb,
+                           self,
+                           NULL,
+                           &error);
+  if (error)
+  {
+    g_warning (G_STRLOC " %s", error->message);
+    g_clear_error (&error);
+  }
+
+  gconf_client_add_dir (priv->client,
+                        EVENT_SOUNDS_DIRECTORY,
+                        GCONF_CLIENT_PRELOAD_ONELEVEL,
+                        &error);
+  if (error)
+  {
+    g_warning (G_STRLOC " %s", error->message);
+    g_clear_error (&error);
+  }
+
+  alert_sounds_button_active = gconf_client_get_bool (priv->client,
+                                                      EVENT_SOUNDS_KEY,
+                                                      &error);
+  if (error)
+  {
+    g_warning (G_STRLOC " %s", error->message);
+    g_clear_error (&error);
+  }
+  _set_alert_sounds_button_active (self, alert_sounds_button_active);
 }
 
 GtkWidget *
@@ -225,14 +354,14 @@ dalston_volume_pane_update_mute (DalstonVolumePane *pane)
 {
   DalstonVolumePanePrivate *priv = GET_PRIVATE (pane);
 
-  /* Block the emission of the toggled signal */
+  /* Block the emission of the switch-flipped signal */
   g_signal_handlers_block_by_func (priv->mute_button,
-                                   _mute_button_toggled_cb,
+                                   _mute_button_switch_flipped_cb,
                                    pane);
-  gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (priv->mute_button),
+  nbtk_gtk_light_switch_set_active (NBTK_GTK_LIGHT_SWITCH (priv->mute_button),
                                 gvc_mixer_stream_get_is_muted (priv->sink));
   g_signal_handlers_unblock_by_func (priv->mute_button,
-                                     _mute_button_toggled_cb,
+                                     _mute_button_switch_flipped_cb,
                                      pane);
   gtk_widget_set_sensitive (priv->volume_slider,
                             !gvc_mixer_stream_get_is_muted (priv->sink));
