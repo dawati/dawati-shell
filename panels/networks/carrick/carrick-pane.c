5cab4f1b32aa9ee22c7064fca4e011a836df3638
Joshua Lock <josh@linux.intel.com>
2009-05-14 10:19:22 +0100

    Never show horizontal scroll bar in service list

--- panels/networks/carrick/carrick-pane.c
+++ panels/networks/carrick/carrick-pane.c
@@ -563,7 +563,7 @@ carrick_pane_init (CarrickPane *self)
   priv->service_list = carrick_list_new ();
   scrolled_view = gtk_scrolled_window_new (NULL, NULL);
   gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (scrolled_view),
-                                  GTK_POLICY_AUTOMATIC,
+                                  GTK_POLICY_NEVER,
                                   GTK_POLICY_AUTOMATIC);
   gtk_scrolled_window_add_with_viewport (GTK_SCROLLED_WINDOW (scrolled_view),
                                          priv->service_list);
