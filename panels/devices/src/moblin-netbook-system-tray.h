06584a608b3ce999f584c5e3c1ed0ae94c42f2db
Rob Bradford <rob@linux.intel.com>
2009-05-18 20:36:30 +0100

    Add LGPLv2.1 headers

--- panels/devices/src/moblin-netbook-system-tray.h
+++ panels/devices/src/moblin-netbook-system-tray.h
@@ -1,30 +1,22 @@
-/* -*- mode: C; c-file-style: "gnu"; indent-tabs-mode: nil; -*- */
-
-/* FIXME -- We can license this under any license we want, as there is no code
- *          in here that links against the plugin in any way. The only thing
- *          that the plugin uses from here is the
- *          MOBLIN_SYSTEM_TRAY_CONFIG_WINDOW define.
- */
-
 /*
- * Copyright (c) 2008 Intel Corp.
+ * Dalston - power and volume applets for Moblin
+ * Copyright (C) 2008, 2009, Intel Corporation.
  *
  * Author: Tomas Frydrych <tf@linux.intel.com>
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of the
- * License, or (at your option) any later version.
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU Lesser General Public License,
+ * version 2.1, as published by the Free Software Foundation.
  *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
+ * This program is distributed in the hope it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
+ * more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
- * 02111-1307, USA.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St - Fifth Floor, Boston, MA 02110-1301 USA.
+ *
  */
 
 #ifndef MOBLIN_NETBOOK_SYSTEM_TRAY_H
