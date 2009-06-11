7921d0e7d44a96451bf2904bbc21bc5808824d1b
Tomas Frydrych <tf@linux.intel.com>
2009-06-11 09:27:32 +0100

    Add file missed in the previous commit

---
+++ libmnb/mnb-panel-common.h
@@ -0,0 +1,27 @@
+
+#ifndef _MNB_PANEL_COMMON
+#define _MNB_PANEL_COMMON
+
+#define MNB_PANEL_DBUS_NAME_PREFIX "org.moblin.Mnb.Panels."
+#define MNB_PANEL_DBUS_PATH_PREFIX "/org/moblin/Mnb/Panels/"
+#define MNB_PANEL_DBUS_INTERFACE   "org.moblin.Mnb.Panel"
+
+#define MNB_TOOLBAR_DBUS_PATH      "/org/moblin/Mnb/Toolbar"
+#define MNB_TOOLBAR_DBUS_NAME      "org.moblin.Mnb.Toolbar"
+#define MNB_TOOLBAR_DBUS_INTERFACE "org.moblin.Mnb.Toolbar"
+
+#define MNB_PANEL_MYZONE       "myzone"
+#define MNB_PANEL_STATUS       "status"
+#define MNB_PANEL_ZONES        "zones"
+#define MNB_PANEL_INTERNET     "internet"
+#define MNB_PANEL_MEDIA        "media"
+#define MNB_PANEL_APPLICATIONS "applications"
+#define MNB_PANEL_PEOPLE       "people"
+#define MNB_PANEL_PASTEBOARD   "pasteboard"
+#define MNB_PANEL_NETWORK      "tray-button-wifi"
+#define MNB_PANEL_BLUETOOTH    "tray-button-bluetooth"
+#define MNB_PANEL_VOLUME       "tray-button-sound"
+#define MNB_PANEL_POWER        "tray-button-battery"
+#define MNB_PANEL_TEST         "test"
+
+#endif
