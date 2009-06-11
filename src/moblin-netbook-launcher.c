df745b315ddee91940aef940399cfee75cbeefce
Tomas Frydrych <tf@linux.intel.com>
2009-06-11 14:02:44 +0100

    [MnbNetbookLauncher] Fix build

--- src/moblin-netbook-launcher.c
+++ src/moblin-netbook-launcher.c
@@ -1407,7 +1407,7 @@ _constructor (GType                  gtype,
   nbtk_scroll_view_set_row_size (NBTK_SCROLL_VIEW (priv->scrollview), SCROLLVIEW_ROW_SIZE);
   bar = nbtk_scroll_view_get_vscroll_bar (NBTK_SCROLL_VIEW (priv->scrollview));
 #if 0
-  /* XXX - the mode was removed from NBTK
+  /* XXX - the mode was removed from NBTK */
   nbtk_scroll_bar_set_mode (NBTK_SCROLL_BAR (bar), NBTK_SCROLL_BAR_MODE_IDLE);
 #endif
   clutter_actor_set_size (priv->scrollview,
