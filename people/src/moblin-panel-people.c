3b0b7b33e15934db437f0f242a86bbca0fdddd11
Rob Bradford <rob@linux.intel.com>
2009-06-23 12:50:38 +0100

    [people] Initialise Clutter/GTK+ in panel as per myzone
    This means that GTK+ and Clutter are initialised together, allowing us to use
    GtkIconTheme. This change also adds a trivial standalone mode for testing.
    
    By initialising GTK+ this fixes MB:#3748

--- people/src/moblin-panel-people.c
+++ people/src/moblin-panel-people.c
@@ -8,6 +8,8 @@
 #include <moblin-panel/mpl-panel-common.h>
 #include "mnb-people-panel.h"
 
+#include <config.h>
+
 static void
 _client_set_size_cb (MplPanelClient *client,
                      guint           width,
@@ -20,6 +22,12 @@ _client_set_size_cb (MplPanelClient *client,
                           height);
 }
 
+static gboolean standalone = FALSE;
+
+static GOptionEntry entries[] = {
+  {"standalone", 's', 0, G_OPTION_ARG_NONE, &standalone, "Do not embed into the mutter-moblin panel", NULL}
+};
+
 int
 main (int    argc,
       char **argv)
@@ -27,30 +35,60 @@ main (int    argc,
   MplPanelClient *client;
   ClutterActor *stage;
   NbtkWidget *people_panel;
+  GOptionContext *context;
+  GError *error = NULL;
 
-  clutter_init (&argc, &argv);
+  context = g_option_context_new ("- mutter-moblin myzone panel");
+  g_option_context_add_main_entries (context, entries, GETTEXT_PACKAGE);
+  g_option_context_add_group (context, clutter_get_option_group_without_init ());
+  g_option_context_add_group (context, gtk_get_option_group (FALSE));
+  if (!g_option_context_parse (context, &argc, &argv, &error))
+  {
+    g_critical (G_STRLOC ": Error parsing option: %s", error->message);
+    g_clear_error (&error);
+  }
+
+  g_option_context_free (context);
+
+  MPL_PANEL_CLUTTER_INIT_WITH_GTK (&argc, &argv);
 
   nbtk_style_load_from_file (nbtk_style_get_default (),
                              MUTTER_MOBLIN_CSS, NULL);
 
-  client = mpl_panel_clutter_new (MPL_PANEL_PEOPLE,
-                                  _("people"),
-                                  MUTTER_MOBLIN_CSS,
-                                  "people-button",
-                                  TRUE);
+  if (!standalone)
+  {
+    client = mpl_panel_clutter_new (MPL_PANEL_PEOPLE,
+                                    _("people"),
+                                    MUTTER_MOBLIN_CSS,
+                                    "people-button",
+                                    TRUE);
 
-  mpl_panel_client_set_height_request (client, 400);
+    mpl_panel_client_set_height_request (client, 400);
 
-  stage = mpl_panel_clutter_get_stage (MPL_PANEL_CLUTTER (client));
-  people_panel = mnb_people_panel_new ();
-  mnb_people_panel_set_panel_client (MNB_PEOPLE_PANEL (people_panel), client);
+    stage = mpl_panel_clutter_get_stage (MPL_PANEL_CLUTTER (client));
+    people_panel = mnb_people_panel_new ();
+    mnb_people_panel_set_panel_client (MNB_PEOPLE_PANEL (people_panel), client);
+    g_signal_connect (client,
+                      "set-size",
+                      (GCallback)_client_set_size_cb,
+                      people_panel);
+  } else {
+    Window xwin;
+
+    stage = clutter_stage_get_default ();
+    clutter_actor_realize (stage);
+    xwin = clutter_x11_get_stage_window (CLUTTER_STAGE (stage));
+
+    MPL_PANEL_CLUTTER_SETUP_EVENTS_WITH_GTK_FOR_XID (xwin);
+    people_panel = mnb_people_panel_new ();
+    clutter_actor_set_size ((ClutterActor *)people_panel, 1016, 400);
+    clutter_actor_set_size (stage, 1016, 400);
+    clutter_actor_show_all (stage);
+  }
 
   clutter_container_add_actor (CLUTTER_CONTAINER (stage),
                                (ClutterActor *)people_panel);
-  g_signal_connect (client,
-                    "set-size",
-                    (GCallback)_client_set_size_cb,
-                    people_panel);
+
 
   clutter_main ();
 
