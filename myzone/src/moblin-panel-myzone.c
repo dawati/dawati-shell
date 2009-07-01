f8c3ff4f1a6f12185a2976b22727297acce0f8a3
Robert Staudinger <robertx.staudinger@intel.com>
2009-07-01 16:46:37 +0200

    [myzone] Split out myzone-specific theme
    Move the myzone's theme bits to a theme of its own and install
    separately.
    
    As per Robster's request the theme bits go with myzone, even though
    they concern penge widgets.

--- myzone/src/moblin-panel-myzone.c
+++ myzone/src/moblin-panel-myzone.c
@@ -62,13 +62,13 @@ main (int    argc,
   MPL_PANEL_CLUTTER_INIT_WITH_GTK (&argc, &argv);
 
   nbtk_style_load_from_file (nbtk_style_get_default (),
-                             MUTTER_MOBLIN_CSS, NULL);
+                             THEMEDIR "/panel.css", NULL);
 
   if (!standalone)
   {
     client = mpl_panel_clutter_new (MPL_PANEL_MYZONE,
                                     _("myzone"),
-                                    MUTTER_MOBLIN_CSS,
+                                    NULL,
                                     "myzone-button",
                                     TRUE);
 
