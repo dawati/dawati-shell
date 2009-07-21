12737dd110e48f6fd5b301a11e7474d6a1d84185
Rob Bradford <rob@linux.intel.com>
2009-07-21 18:59:54 +0100

    Use gnome-common for autogen

--- libanerley/autogen.sh
+++ libanerley/autogen.sh
@@ -1,3 +1,15 @@
-#!/bin/sh
-#gtkdocize --flavour no-tmpl || exit 1
-autoreconf -v -i && ./configure $@
+#!/bin/bash
+# Run this to generate all the initial makefiles, etc.
+
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+PKG_NAME="anerley"
+REQUIRED_AUTOMAKE_VERSION=1.10
+
+which gnome-autogen.sh || {
+    echo "You need to install gnome-common from GNOME Subversion (or from"
+    echo "your distribution's package manager)."
+    exit 1
+}
+USE_GNOME2_MACROS=1 USE_COMMON_DOC_BUILD=yes . gnome-autogen.sh
