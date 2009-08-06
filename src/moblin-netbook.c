b87d5a3bc19313fa88beb211915bc954aa9b1b1a
Tomas Frydrych <tf@linux.intel.com>
2009-08-06 18:19:48 +0100

    Revert "Fix check for presence of applications"
    No longer required due to changes in upstream Mutter.
    
    This reverts commit 1103938bfa27f582c0d13d8b6abc5b07f83a6d80.

--- src/moblin-netbook.c
+++ src/moblin-netbook.c
@@ -859,7 +859,7 @@ check_for_empty_workspace (MutterPlugin *plugin,
        */
       if (!(type == META_COMP_WINDOW_DESKTOP        ||
             type == META_COMP_WINDOW_DOCK           ||
-            mutter_window_is_override_redirect (m)  ||
+            type == META_COMP_WINDOW_OVERRIDE_OTHER ||
             ((mw = mutter_window_get_meta_window (m)) == ignore)))
         {
           g_debug ("Found singificant window %s of type %d",
