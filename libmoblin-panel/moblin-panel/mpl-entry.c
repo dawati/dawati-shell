160b5995bc6e4ce9a7f7c4787383411d55ff4e71
Rob Bradford <rob@linux.intel.com>
2009-08-07 16:34:07 +0100

    [libmoblin-panel] Remove spurious mpl_clutter_theme_ensure_loaded function
    This function was not useful; each panel loads the stylesheet in their own
    panel setup. In particular in the multiple repo case the panels get to choose
    their own arrangement for installing their styling.

--- libmoblin-panel/moblin-panel/mpl-entry.c
+++ libmoblin-panel/moblin-panel/mpl-entry.c
@@ -6,7 +6,6 @@
 #include <glib/gi18n.h>
 #include <stdlib.h>
 
-#include "mpl-clutter-theme.h"
 #include "mpl-entry.h"
 
 #define MPL_ENTRY_GET_PRIVATE(obj) \
@@ -399,8 +398,6 @@ mpl_entry_class_init (MplEntryClass *klass)
                   NULL, NULL,
                   g_cclosure_marshal_VOID__UINT,
                   G_TYPE_NONE, 1, G_TYPE_UINT);
-
-  mpl_clutter_theme_ensure_loaded ();
 }
 
 static void
