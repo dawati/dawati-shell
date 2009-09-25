b600bb92127df9982e665445f52402a3482584bd
Tomas Frydrych <tf@linux.intel.com>
2009-09-25 11:45:28 +0100

    Use raw transiency hint when determining whether to create workspace (MB#6438)
    Since meta_window_get_transient_for() returns MetaWindow, it will return NULL
    if the parent window is not managed; for the purposes of deciding whether
    window should be moved to a new workspace or not, we only care about the
    hint.

--- src/moblin-netbook.c
+++ src/moblin-netbook.c
@@ -1365,7 +1365,7 @@ map (MutterPlugin *plugin, MutterWindow *mcw)
       if (type == META_COMP_WINDOW_NORMAL  &&
           !meta_window_is_modal (mw)       &&
           meta_window_get_startup_id (mw) &&
-          !meta_window_get_transient_for (mw))
+          meta_window_get_transient_for_xid (mw) == None)
         {
           MetaScreen  *screen  = mutter_plugin_get_screen (plugin);
           MetaDisplay *display = meta_screen_get_display (screen);
