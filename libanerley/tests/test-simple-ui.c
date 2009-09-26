0e6e3b7058ff8d89da20d41c19d7b5bd81495d33
Jonny Lamb <jonny.lamb@collabora.co.uk>
2009-09-26 17:45:45 +0100

    test-simple-ui: update to use new account manager/anerley API
    Signed-off-by: Jonny Lamb <jonny.lamb@collabora.co.uk>

--- libanerley/tests/test-simple-ui.c
+++ libanerley/tests/test-simple-ui.c
@@ -26,9 +26,9 @@
 #include <anerley/anerley-tile.h>
 #include <anerley/anerley-aggregate-tp-feed.h>
 #include <anerley/anerley-feed-model.h>
+#include <anerley/anerley-tile-view.h>
 
 #include <clutter/clutter.h>
-#include <libmissioncontrol/mission-control.h>
 #include <telepathy-glib/contact.h>
 
 #include <glib.h>
@@ -49,9 +49,7 @@ int
 main (int    argc,
       char **argv)
 {
-  MissionControl *mc;
-  AnerleyTpFeed *feed;
-  DBusGConnection *conn;
+  AnerleyFeed *feed;
   ClutterActor *stage;
   NbtkWidget *scroll_view;
   NbtkWidget *icon_view;
@@ -84,23 +82,21 @@ main (int    argc,
 
   g_free (path);
 
-  conn = dbus_g_bus_get (DBUS_BUS_SESSION, NULL);
-  mc = mission_control_new (conn);
-  feed = anerley_aggregate_tp_feed_new (mc);
+  feed = anerley_aggregate_tp_feed_new ();
   model = anerley_feed_model_new (feed);
   stage = clutter_stage_get_default ();
-  icon_view = anerley_tile_view_new (model);
+  icon_view = anerley_tile_view_new (ANERLEY_FEED_MODEL (model));
 
   table = nbtk_table_new ();
   entry = nbtk_entry_new (NULL);
-  tmp = nbtk_entry_get_clutter_text (entry);
+  tmp = nbtk_entry_get_clutter_text (NBTK_ENTRY (entry));
   g_signal_connect (tmp,
                     "text-changed",
-                    _entry_text_changed_cb,
+                    G_CALLBACK (_entry_text_changed_cb),
                     model);
 
   nbtk_table_add_actor_with_properties (NBTK_TABLE (table),
-                                        entry,
+                                        CLUTTER_ACTOR (entry),
                                         0,
                                         0,
                                         "x-fill",
@@ -113,12 +109,12 @@ main (int    argc,
 
   scroll_view = nbtk_scroll_view_new ();
   clutter_container_add_actor (CLUTTER_CONTAINER (stage),
-                               table);
+                               CLUTTER_ACTOR (table));
   clutter_container_add_actor (CLUTTER_CONTAINER (scroll_view),
-                               icon_view);
+                               CLUTTER_ACTOR (icon_view));
   clutter_actor_show_all (stage);
   nbtk_table_add_actor_with_properties (NBTK_TABLE (table),
-                                        scroll_view,
+                                        CLUTTER_ACTOR (scroll_view),
                                         1,
                                         0,
                                         "x-fill",
@@ -131,7 +127,7 @@ main (int    argc,
                                         TRUE,
                                         NULL);
 
-  clutter_actor_set_size (table, 640, 480);
+  clutter_actor_set_size (CLUTTER_ACTOR (table), 640, 480);
   clutter_main ();
 
   return 0;
