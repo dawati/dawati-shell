62f8d6f5485c7974b66d9860fc3d02760eab8322
Robert Staudinger <robertx.staudinger@intel.com>
2009-10-07 11:10:17 +0200

    [test-launcher] Fix build after launcher button changes.

--- panels/applications/tests/test-launcher-button.c
+++ panels/applications/tests/test-launcher-button.c
@@ -37,7 +37,7 @@ main (int argc, char *argv[])
 
   launcher = mnb_launcher_button_new ("internet-coloured", "../data/theme/panel/internet-coloured.png",
                                       32, "Launcher Button Launcher Button ", 
-                                      "Category", "Test", "Comment", "/bin/false",
+                                      "Category", "Comment", "/bin/false",
                                       "/usr/share/applications/eog.desktop");
   clutter_actor_set_position (CLUTTER_ACTOR (launcher), 50, 50);
   clutter_actor_set_width (CLUTTER_ACTOR (launcher), 200);
