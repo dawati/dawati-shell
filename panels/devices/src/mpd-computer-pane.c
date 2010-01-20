5bd08946c6c2dfad68aa1cbde28e868687bd1b79
Robert Staudinger <robert.staudinger@intel.com>
2010-01-20 15:38:28 +0100

    Add computer-pane widget for the right column.

---
+++ panels/devices/src/mpd-computer-pane.c
@@ -0,0 +1,60 @@
+
+/*
+ * Copyright (c) 2010 Intel Corp.
+ *
+ * Author: Robert Staudinger <robert.staudinger@intel.com>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU Lesser General Public License,
+ * version 2.1, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St - Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include "mpd-computer-pane.h"
+
+G_DEFINE_TYPE (MpdComputerPane, mpd_computer_pane, MX_TYPE_BOX_LAYOUT)
+
+#define GET_PRIVATE(o) \
+  (G_TYPE_INSTANCE_GET_PRIVATE ((o), MPD_TYPE_COMPUTER_PANE, MpdComputerPanePrivate))
+
+typedef struct
+{
+  int dummy;
+} MpdComputerPanePrivate;
+
+static void
+_dispose (GObject *object)
+{
+  G_OBJECT_CLASS (mpd_computer_pane_parent_class)->dispose (object);
+}
+
+static void
+mpd_computer_pane_class_init (MpdComputerPaneClass *klass)
+{
+  GObjectClass *object_class = G_OBJECT_CLASS (klass);
+
+  g_type_class_add_private (klass, sizeof (MpdComputerPanePrivate));
+
+  object_class->dispose = _dispose;
+}
+
+static void
+mpd_computer_pane_init (MpdComputerPane *self)
+{
+}
+
+MpdComputerPane *
+mpd_computer_pane_new (void)
+{
+  return g_object_new (MPD_TYPE_COMPUTER_PANE, NULL);
+}
+
+
---
+++ panels/devices/src/mpd-computer-pane.h
@@ -0,0 +1,63 @@
+
+/*
+ * Copyright (c) 2010 Intel Corp.
+ *
+ * Author: Robert Staudinger <robert.staudinger@intel.com>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU Lesser General Public License,
+ * version 2.1, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St - Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef MPD_COMPUTER_PANE_H
+#define MPD_COMPUTER_PANE_H
+
+#include <glib-object.h>
+#include <mx/mx.h>
+
+G_BEGIN_DECLS
+
+#define MPD_TYPE_COMPUTER_PANE mpd_computer_pane_get_type()
+
+#define MPD_COMPUTER_PANE(obj) \
+  (G_TYPE_CHECK_INSTANCE_CAST ((obj), MPD_TYPE_COMPUTER_PANE, MpdComputerPane))
+
+#define MPD_COMPUTER_PANE_CLASS(klass) \
+  (G_TYPE_CHECK_CLASS_CAST ((klass), MPD_TYPE_COMPUTER_PANE, MpdComputerPaneClass))
+
+#define MPD_IS_COMPUTER_PANE(obj) \
+  (G_TYPE_CHECK_INSTANCE_TYPE ((obj), MPD_TYPE_COMPUTER_PANE))
+
+#define MPD_IS_COMPUTER_PANE_CLASS(klass) \
+  (G_TYPE_CHECK_CLASS_TYPE ((klass), MPD_TYPE_COMPUTER_PANE))
+
+#define MPD_COMPUTER_PANE_GET_CLASS(obj) \
+  (G_TYPE_INSTANCE_GET_CLASS ((obj), MPD_TYPE_COMPUTER_PANE, MpdComputerPaneClass))
+
+typedef struct
+{
+  MxBoxLayout parent;
+} MpdComputerPane;
+
+typedef struct
+{
+  MxBoxLayoutClass parent;
+} MpdComputerPaneClass;
+
+GType mpd_computer_pane_get_type (void);
+
+MpdComputerPane * mpd_computer_pane_new (void);
+
+G_END_DECLS
+
+#endif /* MPD_COMPUTER_PANE_H */
+
