1b73148c580948c7659c2935b9634dbd0486f921
Rob Bradford <rob@linux.intel.com>
2010-03-29 15:18:49 +0100

    tp-monitor-feed: Synthesise AnerleyTpItem directly
    Rather than look up an item in the existing feeds we instead synthesise a new
    one by a.) finding the account for it and b.) creating a TpContact
    
    This resolves a race in the dbus activation of the People panel by a new
    incoming channel.
    
    Fixes: http://bugzilla.meego.com/show_bug.cgi?id=217

--- libanerley/anerley/anerley-tp-monitor-feed.c
+++ libanerley/anerley/anerley-tp-monitor-feed.c
@@ -139,6 +139,71 @@ _channel_closed_cb (TpChannel *channel,
   g_object_unref (channel);
 }
 
+typedef struct
+{
+  TpChannel *channel;
+  TpAccount *account;
+} GetContactsClosure;
+
+static void
+_tp_connection_get_contacts_cb (TpConnection      *connection,
+                                guint              n_contacts,
+                                TpContact * const *contacts,
+                                guint              n_failed,
+                                const TpHandle    *failed,
+                                const GError      *error,
+                                gpointer           userdata,
+                                GObject           *weak_object)
+{
+  AnerleyTpMonitorFeed *monitor_feed = ANERLEY_TP_MONITIOR_FEED (weak_object);
+  AnerleyTpMonitorFeedPrivate *priv = GET_PRIVATE (monitor_feed);
+  GetContactsClosure *closure = (GetContactsClosure *)userdata;
+  gint i = 0;
+  TpContact *contact;
+  AnerleyTpItem *item;
+  GList *added_items = NULL;
+  TpAccount *account = closure->account;
+  TpChannel *channel = closure->channel;
+
+  if (error)
+  {
+    g_warning (G_STRLOC ": Error getting contacts from handle: %s",
+               error->message);
+    return;
+  }
+
+  /* Only expect 1 */
+  for (i = 0; i < n_contacts; i++)
+  {
+    contact = (TpContact *)contacts[i];
+    item = anerley_tp_item_new (account,
+                                contact);
+    added_items = g_list_append (added_items, item);
+
+    tp_cli_channel_connect_to_closed (channel,
+                                      _channel_closed_cb,
+                                      NULL,
+                                      NULL,
+                                      (GObject *)monitor_feed,
+                                      NULL);
+
+    g_hash_table_insert (priv->channels_to_items,
+                         channel,
+                         g_object_ref (item));
+
+    anerley_tp_item_associate_channel (ANERLEY_TP_ITEM (item), channel);
+  }
+
+  if (added_items)
+    g_signal_emit_by_name (monitor_feed,
+                           "items-added",
+                           added_items);
+
+  g_object_unref (closure->channel);
+  g_object_unref (closure->account);
+  g_free (closure);
+}
+
 static void
 _observer_new_channel_cb (AnerleyTpObserver *observer,
                           const gchar       *account_name,
@@ -148,9 +213,13 @@ _observer_new_channel_cb (AnerleyTpObserver *observer,
   AnerleyTpMonitorFeed *monitor_feed = ANERLEY_TP_MONITIOR_FEED (userdata);
   AnerleyTpMonitorFeedPrivate *priv = GET_PRIVATE (userdata);
   AnerleyFeed *feed;
-  const gchar *uid;
-  AnerleyItem *item;
-  GList *items = NULL;
+  TpHandle contact_handle;
+  TpHandle handles[1] = { 0, };
+  TpContactFeature features[] = {TP_CONTACT_FEATURE_ALIAS,
+                                 TP_CONTACT_FEATURE_AVATAR_TOKEN,
+                                 TP_CONTACT_FEATURE_PRESENCE };
+  TpAccount *account;
+  GetContactsClosure *closure;
 
   feed = anerley_aggregate_tp_feed_get_feed_by_account_name (priv->aggregate_feed,
                                                              account_name);
@@ -164,36 +233,23 @@ _observer_new_channel_cb (AnerleyTpObserver *observer,
     g_debug (G_STRLOC ": Got a valid feed for %s account", account_name);
   }
 
-  uid = tp_channel_get_identifier (channel);
-  item = anerley_tp_feed_get_item_by_uid (ANERLEY_TP_FEED (feed),
-                                          uid);
+  account = anerley_tp_feed_peek_account (ANERLEY_TP_FEED (feed));
+  contact_handle = tp_channel_get_handle (channel, NULL);
+  handles[0] = contact_handle;
 
-  if (!item)
-  {
-    g_warning (G_STRLOC ": Given a channel with no matching item: %s",
-               uid);
-    return;
-  } else {
-    g_debug (G_STRLOC ": Got a valid item for uid %s", uid);
-  }
+  closure = g_new0 (GetContactsClosure, 1);
+  closure->channel = g_object_ref (channel);
+  closure->account = g_object_ref (account);
+  tp_connection_get_contacts_by_handle (tp_channel_borrow_connection (channel),
+                                        1,
+                                        handles,
+                                        3,
+                                        features,
+                                        _tp_connection_get_contacts_cb,
+                                        closure,
+                                        NULL,
+                                        (GObject *)monitor_feed);
 
-  tp_cli_channel_connect_to_closed (channel,
-                                    _channel_closed_cb,
-                                    NULL,
-                                    NULL,
-                                    userdata,
-                                    NULL);
-
-  g_hash_table_insert (priv->channels_to_items,
-                       channel,
-                       g_object_ref (item));
-
-  anerley_tp_item_associate_channel (ANERLEY_TP_ITEM (item), channel);
-
-  items = g_list_append (items, item);
-  g_signal_emit_by_name (monitor_feed,
-                         "items-added",
-                         items);
 }
 
 static void
