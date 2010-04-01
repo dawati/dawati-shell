120a5e90a7c6e8f0498e3260811efdf322f4a63c
Rob Staudinger <robsta@linux.intel.com>
2010-04-01 20:53:09 +0200

    shell: Increase height so everything fits.

--- panels/devices/src/mpd-shell-defines.h
+++ panels/devices/src/mpd-shell-defines.h
@@ -24,7 +24,7 @@
 #define TEXT_COLOR             "#775aaa"
 
 #define MPD_SHELL_WIDTH         1024.0
-#define MPD_SHELL_HEIGHT         480.0
+#define MPD_SHELL_HEIGHT         485.0
 #define MPD_SHELL_PADDING         12.0
 
 #define MPD_PANE_COLUMN_SPACING   14.0
