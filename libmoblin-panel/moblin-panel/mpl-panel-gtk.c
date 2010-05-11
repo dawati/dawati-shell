493f7b3987aab3807c48d50799258af09f3f7e93
Ross Burton <ross@linux.intel.com>
2010-05-11 11:41:32 +0100

    mpl-panel-gtk: set the widget name on the top-level GtkWindow

--- libmoblin-panel/moblin-panel/mpl-panel-gtk.c
+++ libmoblin-panel/moblin-panel/mpl-panel-gtk.c
@@ -172,6 +172,7 @@ mpl_panel_gtk_constructed (GObject *self)
     G_OBJECT_CLASS (mpl_panel_gtk_parent_class)->constructed (self);
 
   priv->window = window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+  gtk_widget_set_name (priv->window, "MplPanelGtk");
   gtk_window_set_type_hint (GTK_WINDOW (window), GDK_WINDOW_TYPE_HINT_DOCK);
   gtk_window_stick (GTK_WINDOW (window));
 
