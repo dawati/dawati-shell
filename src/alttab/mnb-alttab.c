meego-netbook-ux/mutter-meego

Port to upstream mutter API

src/alttab/mnb-alttab.c
@@ -45,7 +45,7 @@
 #include "../meego-netbook.h"
 #include "../mnb-toolbar.h"
 
-#include <display.h>
+#include <meta/display.h>
 
 /*
  * Establishes an active kbd grab for us via the Meta API.
@@ -100,11 +100,11 @@ void
 end_kbd_grab (MnbAlttabOverlay *overlay)
 {
   MnbAlttabOverlayPrivate *priv   = overlay->priv;
-  MutterPlugin            *plugin = meego_netbook_get_plugin_singleton ();
+  MetaPlugin              *plugin = meego_netbook_get_plugin_singleton ();
 
   if (priv->in_alt_grab)
     {
-      MetaScreen  *screen  = mutter_plugin_get_screen (plugin);
+      MetaScreen  *screen  = meta_plugin_get_screen (plugin);
       MetaDisplay *display = meta_screen_get_display (screen);
       guint        timestamp;
 
@@ -146,9 +146,9 @@ alt_tab_initial_timeout_cb (gpointer data)
   ClutterActor              *stage;
   Window                     xwin;
   MnbAlttabOverlayPrivate   *priv = alt_data->overlay->priv;
-  MutterPlugin              *plugin = meego_netbook_get_plugin_singleton ();
+  MetaPlugin                *plugin = meego_netbook_get_plugin_singleton ();
 
-  stage = mutter_get_stage_for_screen (alt_data->screen);
+  stage = meta_get_stage_for_screen (alt_data->screen);
   xwin  = clutter_x11_get_stage_window (CLUTTER_STAGE (stage));
 
   priv->waiting_for_timeout = FALSE;
@@ -202,30 +202,31 @@ alt_tab_initial_timeout_cb (gpointer data)
 static gboolean
 applications_present (void)
 {
-  MutterPlugin               *plugin = meego_netbook_get_plugin_singleton ();
-  MetaScreen                 *screen = mutter_plugin_get_screen (plugin);
-  gint                        count  = 0;
-  GList                      *l;
+  MetaPlugin               *plugin = meego_netbook_get_plugin_singleton ();
+  MetaScreen               *screen = meta_plugin_get_screen (plugin);
+  gint                      count  = 0;
+  GList                    *l;
 
   /*
    * Check for running applications; we do this by checking if any
    * application-type windows are present.
    */
-  l = mutter_get_windows (screen);
+  l = meta_get_window_actors (screen);
 
   while (l)
     {
-      MutterWindow       *m    = l->data;
-      MetaCompWindowType  type = mutter_window_get_window_type (m);
+      MetaWindowActor     *m   = l->data;
+      MetaWindow          *w   = meta_window_actor_get_meta_window (m);
+      MetaWindowType       type = meta_window_get_window_type (w);
 
       /*
        * Ignore desktop, docs, and panel windows
        *
        * (Panel windows are currently of type META_COMP_WINDOW_OVERRIDE_OTHER)
        */
-      if (!(type == META_COMP_WINDOW_DESKTOP        ||
-            type == META_COMP_WINDOW_DOCK           ||
-            type == META_COMP_WINDOW_OVERRIDE_OTHER))
+      if (!(type == META_WINDOW_DESKTOP        ||
+            type == META_WINDOW_DOCK           ||
+            type == META_WINDOW_OVERRIDE_OTHER))
         {
           if (++count >= 2)
             break;
@@ -253,7 +254,7 @@ mnb_alttab_overlay_alt_tab_key_handler (MetaDisplay    *display,
 {
   MnbAlttabOverlay           *overlay = MNB_ALTTAB_OVERLAY (data);
   MnbAlttabOverlayPrivate    *priv    = overlay->priv;
-  MutterPlugin               *plugin  = meego_netbook_get_plugin_singleton ();
+  MetaPlugin                 *plugin  = meego_netbook_get_plugin_singleton ();
   MetaWindow                 *focus;
 
   if (meego_netbook_urgent_notification_present (plugin))
@@ -457,7 +458,7 @@ mnb_alttab_overlay_alt_tab_select_handler (MetaDisplay    *display,
 {
   MnbAlttabOverlay           *overlay = MNB_ALTTAB_OVERLAY (data);
   MnbAlttabOverlayPrivate    *priv    = overlay->priv;
-  MutterPlugin               *plugin  = meego_netbook_get_plugin_singleton ();
+  MetaPlugin                 *plugin  = meego_netbook_get_plugin_singleton ();
 
   end_kbd_grab (overlay);
 

src/mnb-panel.c
@@ -423,7 +423,7 @@ mnb_panel_hide_with_toolbar_hide_completed_cb (MnbPanel         *panel,
 void
 mnb_panel_hide_with_toolbar (MnbPanel *panel, MnbShowHideReason reason)
 {
-  MutterPlugin *plugin  = meego_netbook_get_plugin_singleton ();
+  MetaPlugin *plugin  = meego_netbook_get_plugin_singleton ();
   ClutterActor *toolbar = meego_netbook_get_toolbar (plugin);
 
   if (!mnb_panel_is_mapped (panel))
@@ -452,10 +452,10 @@ mnb_panel_ensure_size (MnbPanel *panel)
   MetaRectangle  r;
   MetaScreen    *screen;
   MetaWorkspace *workspace;
-  MutterPlugin  *plugin = meego_netbook_get_plugin_singleton ();
+  MetaPlugin    *plugin = meego_netbook_get_plugin_singleton ();
   gboolean       netbook_mode = meego_netbook_use_netbook_mode (plugin);
 
-  screen    = mutter_plugin_get_screen (plugin);
+  screen    = meta_plugin_get_screen (plugin);
   workspace = meta_screen_get_active_workspace (screen);
 
   if (workspace)

src/mnb-toolbar-clock.c
@@ -67,7 +67,7 @@ mnb_toolbar_clock_dispose (GObject *object)
 
   if (priv->toolbar_show_id)
     {
-      MutterPlugin *plugin = meego_netbook_get_plugin_singleton ();
+      MetaPlugin *plugin = meego_netbook_get_plugin_singleton ();
       MeegoNetbookPluginPrivate *ppriv = MEEGO_NETBOOK_PLUGIN (plugin)->priv;
 
       g_signal_handler_disconnect (ppriv->toolbar, priv->toolbar_show_id);
@@ -273,7 +273,7 @@ mnb_toolbar_clock_map (ClutterActor *actor)
    */
   if (!priv->toolbar_show_id)
     {
-      MutterPlugin *plugin = meego_netbook_get_plugin_singleton ();
+      MetaPlugin *plugin = meego_netbook_get_plugin_singleton ();
       MeegoNetbookPluginPrivate *ppriv  = MEEGO_NETBOOK_PLUGIN (plugin)->priv;
 
       priv->toolbar_show_id =

src/notifications/ntf-overlay.c
@@ -218,11 +218,11 @@ ntf_overlay_get_preferred_width (ClutterActor *actor,
                                  gfloat       *min_width,
                                  gfloat       *natural_width)
 {
-  MutterPlugin *plugin = meego_netbook_get_plugin_singleton ();
+  MetaPlugin   *plugin = meego_netbook_get_plugin_singleton ();
   gint          screen_width;
   gint          screen_height;
 
-  mutter_plugin_query_screen_size (plugin, &screen_width, &screen_height);
+  meta_plugin_query_screen_size (plugin, &screen_width, &screen_height);
 
   *min_width     = screen_width;
   *natural_width = screen_width;
@@ -234,11 +234,11 @@ ntf_overlay_get_preferred_height (ClutterActor *actor,
                                gfloat       *min_height,
                                gfloat       *natural_height)
 {
-  MutterPlugin *plugin = meego_netbook_get_plugin_singleton ();
+  MetaPlugin   *plugin = meego_netbook_get_plugin_singleton ();
   gint          screen_width;
   gint          screen_height;
 
-  mutter_plugin_query_screen_size (plugin, &screen_width, &screen_height);
+  meta_plugin_query_screen_size (plugin, &screen_width, &screen_height);
 
   *min_height     = screen_height;
   *natural_height = screen_height;

src/notifications/ntf-source.c
@@ -366,8 +366,8 @@ ntf_source_new_for_window (MetaWindow *window)
 NtfSource *
 ntf_source_new_for_pid (const gchar *machine, gint pid)
 {
-  MutterPlugin *plugin = meego_netbook_get_plugin_singleton ();
-  MetaScreen   *screen = mutter_plugin_get_screen (plugin);
+  MetaPlugin   *plugin = meego_netbook_get_plugin_singleton ();
+  MetaScreen   *screen = meta_plugin_get_screen (plugin);
   gchar        *id;
   NtfSource    *src;
   MetaWindow   *window = NULL;
@@ -376,12 +376,12 @@ ntf_source_new_for_pid (const gchar *machine, gint pid)
   g_return_val_if_fail (pid, NULL);
 
 
-  for (l = mutter_get_windows (screen); l; l = l->next)
+  for (l = meta_get_window_actors (screen); l; l = l->next)
     {
-      MutterWindow *m = l->data;
-      MetaWindow   *w = mutter_window_get_meta_window (m);
-      const gchar  *c = meta_window_get_client_machine (w);
-      gint          p = meta_window_get_pid (w);
+      MetaWindowActor *m = l->data;
+      MetaWindow      *w = meta_window_actor_get_meta_window (m);
+      const gchar     *c = meta_window_get_client_machine (w);
+      gint             p = meta_window_get_pid (w);
 
       if (p != pid)
         continue;

src/notifications/ntf-wm.c
@@ -24,7 +24,7 @@
 #include <string.h>
 #include <glib/gi18n.h>
 
-#include <display.h>
+#include <meta/display.h>
 
 #include "../meego-netbook.h"
 #include "ntf-notification.h"
@@ -204,23 +204,23 @@ ntf_wm_demands_attention_clear (MetaWindow *window)
 static void
 ntf_wm_display_window_demands_attention_cb (MetaDisplay  *display,
                                             MetaWindow   *mw,
-                                            MutterPlugin *plugin)
+                                            MetaPlugin   *plugin)
 {
-  MutterWindow       *mcw;
-  MetaCompWindowType  type;
+  MetaWindowActor *mcw;
+  MetaWindowType   type;
 
-  mcw = (MutterWindow*)meta_window_get_compositor_private (mw);
+  mcw = (MetaWindowActor*)meta_window_get_compositor_private (mw);
 
   g_return_if_fail (mcw);
 
   /*
    * Only use notifications for normal windows and dialogues.
    */
-  type = mutter_window_get_window_type (mcw);
+  type = meta_window_get_window_type (mw);
 
-  if (!(type == META_COMP_WINDOW_NORMAL       ||
-        type == META_COMP_WINDOW_MODAL_DIALOG ||
-        type == META_COMP_WINDOW_DIALOG))
+  if (!(type == META_WINDOW_NORMAL       ||
+        type == META_WINDOW_MODAL_DIALOG ||
+        type == META_WINDOW_DIALOG))
     {
       return;
     }
@@ -232,7 +232,7 @@ ntf_wm_display_window_demands_attention_cb (MetaDisplay  *display,
 static void
 ntf_wm_display_focus_window_notify_cb (MetaDisplay  *display,
                                        GParamSpec   *spec,
-                                       MutterPlugin *plugin)
+                                       MetaPlugin   *plugin)
 {
   MetaWindow *mw = meta_display_get_focus_window (display);
 
@@ -243,9 +243,9 @@ ntf_wm_display_focus_window_notify_cb (MetaDisplay  *display,
 void
 ntf_wm_init (void)
 {
-  MutterPlugin *plugin  = meego_netbook_get_plugin_singleton ();
-  MetaScreen   *screen  = mutter_plugin_get_screen (plugin);
-  MetaDisplay  *display = meta_screen_get_display (screen);
+  MetaPlugin  *plugin  = meego_netbook_get_plugin_singleton ();
+  MetaScreen  *screen  = meta_plugin_get_screen (plugin);
+  MetaDisplay *display = meta_screen_get_display (screen);
 
   subsystem_id = ntf_notification_get_subsystem_id ();
 

dawati/dawati-shell

src/alttab/mnb-alttab.c
@@ -45,7 +45,7 @@
 #include "../dawati-netbook.h"
 #include "../mnb-toolbar.h"
 
-#include <display.h>
+#include <meta/display.h>
 
 /*
  * Establishes an active kbd grab for us via the Meta API.
@@ -100,11 +100,11 @@ void
 end_kbd_grab (MnbAlttabOverlay *overlay)
 {
   MnbAlttabOverlayPrivate *priv   = overlay->priv;
-  MutterPlugin            *plugin = dawati_netbook_get_plugin_singleton ();
+  MetaPlugin              *plugin = dawati_netbook_get_plugin_singleton ();
 
   if (priv->in_alt_grab)
     {
-      MetaScreen  *screen  = mutter_plugin_get_screen (plugin);
+      MetaScreen  *screen  = meta_plugin_get_screen (plugin);
       MetaDisplay *display = meta_screen_get_display (screen);
       guint        timestamp;
 
@@ -146,9 +146,9 @@ alt_tab_initial_timeout_cb (gpointer data)
   ClutterActor              *stage;
   Window                     xwin;
   MnbAlttabOverlayPrivate   *priv = alt_data->overlay->priv;
-  MutterPlugin              *plugin = dawati_netbook_get_plugin_singleton ();
+  MetaPlugin                *plugin = dawati_netbook_get_plugin_singleton ();
 
-  stage = mutter_get_stage_for_screen (alt_data->screen);
+  stage = meta_get_stage_for_screen (alt_data->screen);
   xwin  = clutter_x11_get_stage_window (CLUTTER_STAGE (stage));
 
   priv->waiting_for_timeout = FALSE;
@@ -202,30 +202,31 @@ alt_tab_initial_timeout_cb (gpointer data)
 static gboolean
 applications_present (void)
 {
-  MutterPlugin               *plugin = dawati_netbook_get_plugin_singleton ();
-  MetaScreen                 *screen = mutter_plugin_get_screen (plugin);
-  gint                        count  = 0;
-  GList                      *l;
+  MetaPlugin               *plugin = dawati_netbook_get_plugin_singleton ();
+  MetaScreen               *screen = meta_plugin_get_screen (plugin);
+  gint                      count  = 0;
+  GList                    *l;
 
   /*
    * Check for running applications; we do this by checking if any
    * application-type windows are present.
    */
-  l = mutter_get_windows (screen);
+  l = meta_get_window_actors (screen);
 
   while (l)
     {
-      MutterWindow       *m    = l->data;
-      MetaCompWindowType  type = mutter_window_get_window_type (m);
+      MetaWindowActor     *m   = l->data;
+      MetaWindow          *w   = meta_window_actor_get_meta_window (m);
+      MetaWindowType       type = meta_window_get_window_type (w);
 
       /*
        * Ignore desktop, docs, and panel windows
        *
        * (Panel windows are currently of type META_COMP_WINDOW_OVERRIDE_OTHER)
        */
-      if (!(type == META_COMP_WINDOW_DESKTOP        ||
-            type == META_COMP_WINDOW_DOCK           ||
-            type == META_COMP_WINDOW_OVERRIDE_OTHER))
+      if (!(type == META_WINDOW_DESKTOP        ||
+            type == META_WINDOW_DOCK           ||
+            type == META_WINDOW_OVERRIDE_OTHER))
         {
           if (++count >= 2)
             break;
@@ -253,7 +254,7 @@ mnb_alttab_overlay_alt_tab_key_handler (MetaDisplay    *display,
 {
   MnbAlttabOverlay           *overlay = MNB_ALTTAB_OVERLAY (data);
   MnbAlttabOverlayPrivate    *priv    = overlay->priv;
-  MutterPlugin               *plugin  = dawati_netbook_get_plugin_singleton ();
+  MetaPlugin                 *plugin  = dawati_netbook_get_plugin_singleton ();
   MetaWindow                 *focus;
 
   if (dawati_netbook_urgent_notification_present (plugin))
@@ -462,7 +463,7 @@ mnb_alttab_overlay_alt_tab_select_handler (MetaDisplay    *display,
 {
   MnbAlttabOverlay           *overlay = MNB_ALTTAB_OVERLAY (data);
   MnbAlttabOverlayPrivate    *priv    = overlay->priv;
-  MutterPlugin               *plugin  = dawati_netbook_get_plugin_singleton ();
+  MetaPlugin                 *plugin  = dawati_netbook_get_plugin_singleton ();
 
   end_kbd_grab (overlay);
 

src/mnb-panel.c
@@ -423,7 +423,7 @@ mnb_panel_hide_with_toolbar_hide_completed_cb (MnbPanel         *panel,
 void
 mnb_panel_hide_with_toolbar (MnbPanel *panel, MnbShowHideReason reason)
 {
-  MutterPlugin *plugin  = dawati_netbook_get_plugin_singleton ();
+  MetaPlugin *plugin  = dawati_netbook_get_plugin_singleton ();
   ClutterActor *toolbar = dawati_netbook_get_toolbar (plugin);
 
   if (!mnb_panel_is_mapped (panel))
@@ -452,10 +452,10 @@ mnb_panel_ensure_size (MnbPanel *panel)
   MetaRectangle  r;
   MetaScreen    *screen;
   MetaWorkspace *workspace;
-  MutterPlugin  *plugin = dawati_netbook_get_plugin_singleton ();
+  MetaPlugin    *plugin = dawati_netbook_get_plugin_singleton ();
   gboolean       netbook_mode = dawati_netbook_use_netbook_mode (plugin);
 
-  screen    = mutter_plugin_get_screen (plugin);
+  screen    = meta_plugin_get_screen (plugin);
   workspace = meta_screen_get_active_workspace (screen);
 
   if (workspace)

src/mnb-toolbar-clock.c
@@ -67,7 +67,7 @@ mnb_toolbar_clock_dispose (GObject *object)
 
   if (priv->toolbar_show_id)
     {
-      MutterPlugin *plugin = dawati_netbook_get_plugin_singleton ();
+      MetaPlugin *plugin = dawati_netbook_get_plugin_singleton ();
       DawatiNetbookPluginPrivate *ppriv = DAWATI_NETBOOK_PLUGIN (plugin)->priv;
 
       g_signal_handler_disconnect (ppriv->toolbar, priv->toolbar_show_id);
@@ -273,7 +273,7 @@ mnb_toolbar_clock_map (ClutterActor *actor)
    */
   if (!priv->toolbar_show_id)
     {
-      MutterPlugin *plugin = dawati_netbook_get_plugin_singleton ();
+      MetaPlugin *plugin = dawati_netbook_get_plugin_singleton ();
       DawatiNetbookPluginPrivate *ppriv  = DAWATI_NETBOOK_PLUGIN (plugin)->priv;
 
       priv->toolbar_show_id =

src/notifications/ntf-overlay.c
@@ -218,11 +218,11 @@ ntf_overlay_get_preferred_width (ClutterActor *actor,
                                  gfloat       *min_width,
                                  gfloat       *natural_width)
 {
-  MutterPlugin *plugin = dawati_netbook_get_plugin_singleton ();
+  MetaPlugin   *plugin = dawati_netbook_get_plugin_singleton ();
   gint          screen_width;
   gint          screen_height;
 
-  mutter_plugin_query_screen_size (plugin, &screen_width, &screen_height);
+  meta_plugin_query_screen_size (plugin, &screen_width, &screen_height);
 
   *min_width     = screen_width;
   *natural_width = screen_width;
@@ -234,11 +234,11 @@ ntf_overlay_get_preferred_height (ClutterActor *actor,
                                gfloat       *min_height,
                                gfloat       *natural_height)
 {
-  MutterPlugin *plugin = dawati_netbook_get_plugin_singleton ();
+  MetaPlugin   *plugin = dawati_netbook_get_plugin_singleton ();
   gint          screen_width;
   gint          screen_height;
 
-  mutter_plugin_query_screen_size (plugin, &screen_width, &screen_height);
+  meta_plugin_query_screen_size (plugin, &screen_width, &screen_height);
 
   *min_height     = screen_height;
   *natural_height = screen_height;

src/notifications/ntf-source.c
@@ -366,8 +366,8 @@ ntf_source_new_for_window (MetaWindow *window)
 NtfSource *
 ntf_source_new_for_pid (const gchar *machine, gint pid)
 {
-  MutterPlugin *plugin = dawati_netbook_get_plugin_singleton ();
-  MetaScreen   *screen = mutter_plugin_get_screen (plugin);
+  MetaPlugin   *plugin = dawati_netbook_get_plugin_singleton ();
+  MetaScreen   *screen = meta_plugin_get_screen (plugin);
   gchar        *id;
   NtfSource    *src;
   MetaWindow   *window = NULL;
@@ -376,12 +376,12 @@ ntf_source_new_for_pid (const gchar *machine, gint pid)
   g_return_val_if_fail (pid, NULL);
 
 
-  for (l = mutter_get_windows (screen); l; l = l->next)
+  for (l = meta_get_window_actors (screen); l; l = l->next)
     {
-      MutterWindow *m = l->data;
-      MetaWindow   *w = mutter_window_get_meta_window (m);
-      const gchar  *c = meta_window_get_client_machine (w);
-      gint          p = meta_window_get_pid (w);
+      MetaWindowActor *m = l->data;
+      MetaWindow      *w = meta_window_actor_get_meta_window (m);
+      const gchar     *c = meta_window_get_client_machine (w);
+      gint             p = meta_window_get_pid (w);
 
       if (p != pid)
         continue;

src/notifications/ntf-wm.c
@@ -24,7 +24,7 @@
 #include <string.h>
 #include <glib/gi18n.h>
 
-#include <display.h>
+#include <meta/display.h>
 
 #include "../dawati-netbook.h"
 #include "ntf-notification.h"
@@ -220,23 +220,23 @@ ntf_wm_demands_attention_clear (MetaWindow *window)
 static void
 ntf_wm_display_window_demands_attention_cb (MetaDisplay  *display,
                                             MetaWindow   *mw,
-                                            MutterPlugin *plugin)
+                                            MetaPlugin   *plugin)
 {
-  MutterWindow       *mcw;
-  MetaCompWindowType  type;
+  MetaWindowActor *mcw;
+  MetaWindowType   type;
 
-  mcw = (MutterWindow*)meta_window_get_compositor_private (mw);
+  mcw = (MetaWindowActor*)meta_window_get_compositor_private (mw);
 
   g_return_if_fail (mcw);
 
   /*
    * Only use notifications for normal windows and dialogues.
    */
-  type = mutter_window_get_window_type (mcw);
+  type = meta_window_get_window_type (mw);
 
-  if (!(type == META_COMP_WINDOW_NORMAL       ||
-        type == META_COMP_WINDOW_MODAL_DIALOG ||
-        type == META_COMP_WINDOW_DIALOG))
+  if (!(type == META_WINDOW_NORMAL       ||
+        type == META_WINDOW_MODAL_DIALOG ||
+        type == META_WINDOW_DIALOG))
     {
       return;
     }
@@ -248,7 +248,7 @@ ntf_wm_display_window_demands_attention_cb (MetaDisplay  *display,
 static void
 ntf_wm_display_focus_window_notify_cb (MetaDisplay  *display,
                                        GParamSpec   *spec,
-                                       MutterPlugin *plugin)
+                                       MetaPlugin   *plugin)
 {
   MetaWindow *mw = meta_display_get_focus_window (display);
 
@@ -259,9 +259,9 @@ ntf_wm_display_focus_window_notify_cb (MetaDisplay  *display,
 void
 ntf_wm_init (void)
 {
-  MutterPlugin *plugin  = dawati_netbook_get_plugin_singleton ();
-  MetaScreen   *screen  = mutter_plugin_get_screen (plugin);
-  MetaDisplay  *display = meta_screen_get_display (screen);
+  MetaPlugin  *plugin  = dawati_netbook_get_plugin_singleton ();
+  MetaScreen  *screen  = meta_plugin_get_screen (plugin);
+  MetaDisplay *display = meta_screen_get_display (screen);
 
   subsystem_id = ntf_notification_get_subsystem_id ();
 