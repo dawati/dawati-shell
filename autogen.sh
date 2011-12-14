32ed31d63d1c4a6ce8f43ec05ff72a959970b08e
Damien Lespiau <damien.lespiau@intel.com>
2011-12-14 12:17:32 +0000

    build: Change the package name in autogen to dawati-shell

--- autogen.sh
+++ autogen.sh
@@ -4,13 +4,13 @@
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
-PKG_NAME="mutter-dawati"
+PKG_NAME="dawati-shell"
 REQUIRED_AUTOMAKE_VERSION=1.10
 
 (test -f $srcdir/configure.ac \
   && test -d $srcdir/src) || {
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
-    echo " top-level mutter-dawati directory"
+    echo " top-level $PKG_NAME directory"
     exit 1
 }
 
