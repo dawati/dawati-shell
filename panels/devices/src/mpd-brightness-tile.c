03c4eead3450663d67a9ec1602db5e80e837bf5d
Damien Lespiau <damien.lespiau@intel.com>
2012-01-27 15:17:29 +0000

    devices: Implement the new design for the brightness slider

--- panels/devices/src/mpd-brightness-tile.c
+++ panels/devices/src/mpd-brightness-tile.c
@@ -35,8 +35,6 @@ G_DEFINE_TYPE (MpdBrightnessTile, mpd_brightness_tile, MX_TYPE_BOX_LAYOUT)
 
 typedef struct
 {
-  MxLabel             *header;
-  ClutterActor        *bars;
   MxSlider            *slider;
   MpdBatteryDevice    *battery;
   MpdDisplayDevice    *display;
@@ -65,49 +63,6 @@ get_display_device_mode (MpdBrightnessTile *self)
   }
 
   return mode;
-}
-
-static char *
-build_icon_name_15 (char const *template,
-                    float       value,
-                    char const *suffix)
-{
-  char *name;
-
-  value = CLAMP (value, 0.0, 1.0);
-
-  if (value < 0.067)
-    name = g_strdup_printf ("%s7.%s", template, suffix);
-  else if (value < 0.133)
-    name = g_strdup_printf ("%s13.%s", template, suffix);
-  else if (value < 0.200)
-    name = g_strdup_printf ("%s20.%s", template, suffix);
-  else if (value < 0.267)
-    name = g_strdup_printf ("%s27.%s", template, suffix);
-  else if (value < 0.333)
-    name = g_strdup_printf ("%s33.%s", template, suffix);
-  else if (value < 0.400)
-    name = g_strdup_printf ("%s40.%s", template, suffix);
-  else if (value < 0.467)
-    name = g_strdup_printf ("%s47.%s", template, suffix);
-  else if (value < 0.533)
-    name = g_strdup_printf ("%s53.%s", template, suffix);
-  else if (value < 0.600)
-    name = g_strdup_printf ("%s60.%s", template, suffix);
-  else if (value < 0.667)
-    name = g_strdup_printf ("%s67.%s", template, suffix);
-  else if (value < 0.733)
-    name = g_strdup_printf ("%s73.%s", template, suffix);
-  else if (value < 0.800)
-    name = g_strdup_printf ("%s80.%s", template, suffix);
-  else if (value < 0.867)
-    name = g_strdup_printf ("%s87.%s", template, suffix);
-  else if (value < 0.933)
-    name = g_strdup_printf ("%s93.%s", template, suffix);
-  else
-    name = g_strdup_printf ("%s100.%s", template, suffix);
-
-  return name;
 }
 
 static void
@@ -169,59 +124,29 @@ static void
 mpd_brightness_tile_init (MpdBrightnessTile *self)
 {
   MpdBrightnessTilePrivate *priv = GET_PRIVATE (self);
-  ClutterActor  *hbox;
   ClutterActor  *icon;
-  ClutterActor  *vbox;
-  GError        *error = NULL;
 
-  mx_box_layout_set_orientation (MX_BOX_LAYOUT (self), MX_ORIENTATION_VERTICAL);
-  mx_box_layout_set_spacing (MX_BOX_LAYOUT (self), MPD_TILE_HEADER_SPACING);
+  /* Layout  */
+  mx_box_layout_set_spacing (MX_BOX_LAYOUT (self), MPD_TILE_ICON_SPACING);
 
-  /* First row. */
-  priv->header = (MxLabel *) mx_label_new ();
-  clutter_actor_set_name (CLUTTER_ACTOR (priv->header),
-                          "brightness-tile-label");
-  clutter_container_add_actor (CLUTTER_CONTAINER (self),
-                               CLUTTER_ACTOR (priv->header));
-
-  /* Second row. */
-  hbox = mx_box_layout_new ();
-  mx_box_layout_set_spacing (MX_BOX_LAYOUT (hbox), MPD_TILE_ICON_SPACING);
-  clutter_container_add_actor (CLUTTER_CONTAINER (self), hbox);
-
-  icon = clutter_texture_new_from_file (PKGICONDIR "/brightness-icon.png",
-                                        &error);
-  if (error)
-  {
-    g_warning ("%s : %s", G_STRLOC, error->message);
-    g_clear_error (&error);
-  } else {
-    clutter_texture_set_sync_size (CLUTTER_TEXTURE (icon), true);
-    clutter_container_add_actor (CLUTTER_CONTAINER (hbox), icon);
-  }
-
-  vbox = mx_box_layout_new ();
-  mx_box_layout_set_orientation (MX_BOX_LAYOUT (vbox), MX_ORIENTATION_VERTICAL);
-  mx_box_layout_set_spacing (MX_BOX_LAYOUT (vbox), MPD_TILE_BAR_SPACING);
-  clutter_container_add_actor (CLUTTER_CONTAINER (hbox), vbox);
-
-  priv->bars = clutter_texture_new ();
-  clutter_texture_set_sync_size (CLUTTER_TEXTURE (priv->bars), true);
-  clutter_container_add_actor (CLUTTER_CONTAINER (vbox), priv->bars);
+  icon = mx_icon_new ();
+  clutter_actor_set_name (icon, "brightness-off");
+  clutter_container_add_actor (CLUTTER_CONTAINER (self), icon);
 
   priv->slider = (MxSlider *) mx_slider_new ();
+  mx_box_layout_add_actor_with_properties (MX_BOX_LAYOUT (self),
+                                           (ClutterActor *) priv->slider,
+                                           -1,
+                                           "expand", TRUE,
+                                           NULL);
   g_signal_connect (priv->slider, "notify::value",
                     G_CALLBACK (_brightness_slider_value_notify_cb), self);
-  clutter_container_add_actor (CLUTTER_CONTAINER (vbox),
-                               CLUTTER_ACTOR (priv->slider));
-  clutter_container_child_set (CLUTTER_CONTAINER (vbox),
-                               CLUTTER_ACTOR (priv->slider),
-                                "expand", true,
-                                "x-fill", true,
-                                "y-fill", false,
-                                NULL);
 
+  icon = mx_icon_new ();
+  clutter_actor_set_name (icon, "brightness-on");
+  clutter_container_add_actor (CLUTTER_CONTAINER (self), icon);
 
+  /* Hook the logic */
   priv->display = mpd_display_device_new ();
   g_signal_connect (priv->display, "notify::brightness",
                     G_CALLBACK (_display_brightness_notify_cb), self);
@@ -237,29 +162,6 @@ ClutterActor *
 mpd_brightness_tile_new (void)
 {
   return g_object_new (MPD_TYPE_BRIGHTNESS_TILE, NULL);
-}
-
-static void
-update_brightness_bars (MpdBrightnessTile *self,
-                        float              brightness)
-{
-  MpdBrightnessTilePrivate *priv = GET_PRIVATE (self);
-  char          *icon_file;
-  GError        *error = NULL;
-
-  icon_file = build_icon_name_15 (PKGICONDIR "/brightness-bars-",
-                                  brightness,
-                                  "png");
-  clutter_texture_set_from_file (CLUTTER_TEXTURE (priv->bars),
-                                 icon_file,
-                                 &error);
-  g_free (icon_file);
-
-  if (error)
-  {
-    g_warning ("%s : %s", G_STRLOC, error->message);
-    g_clear_error (&error);
-  }
 }
 
 static void
@@ -279,25 +181,13 @@ update_display_brightness (MpdBrightnessTile *self)
 
   g_signal_connect (priv->display, "notify::brightness",
                     G_CALLBACK (_display_brightness_notify_cb), self);
-
-  update_brightness_bars (self, brightness);
 }
 
 static void
 update_brightness_display (MpdBrightnessTile *self)
 {
   MpdBrightnessTilePrivate *priv = GET_PRIVATE (self);
-  MpdBatteryDeviceState battery_state;
-  float                 brightness;
-
-  battery_state = mpd_battery_device_get_state (priv->battery);
-  if (battery_state == MPD_BATTERY_DEVICE_STATE_CHARGING ||
-      battery_state == MPD_BATTERY_DEVICE_STATE_FULLY_CHARGED)
-  {
-    mx_label_set_text (priv->header, _("Display brightness - plugged in"));
-  } else {
-    mx_label_set_text (priv->header, _("Display brightness - on battery"));
-  }
+  float brightness;
 
   g_signal_handlers_disconnect_by_func (priv->slider,
                                         _brightness_slider_value_notify_cb,
@@ -311,7 +201,5 @@ update_brightness_display (MpdBrightnessTile *self)
 
   g_signal_connect (priv->slider, "notify::value",
                     G_CALLBACK (_brightness_slider_value_notify_cb), self);
-
-  update_brightness_bars (self, brightness);
 }
 
