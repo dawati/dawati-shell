9ab6b25eb0a541025dee2a51e393c53c404d1d0b
Cosimo Alfarano <cosimo.alfarano@collabora.com>
2012-02-02 12:30:42 +0000

    MyZone: setting panel name to Myzone (not MyZone)

--- penge/penge-grid-view.c
+++ penge/penge-grid-view.c
@@ -476,7 +476,7 @@ penge_grid_view_init (PengeGridView *self)
 
   self->priv = priv;
 
-  priv->header_label = mx_label_new_with_text ("MyZone");
+  priv->header_label = mx_label_new_with_text ("Myzone");
   mx_stylable_set_style_class (MX_STYLABLE (priv->header_label), "titleBar");
   mx_table_add_actor_with_properties (MX_TABLE (self),
                                       priv->header_label,
